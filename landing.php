751e115df4923395c66611d5d1aaf97d7265f5cc
juan ramirez <john_albert_86@live.com>
2023-04-09 23:40:04 -0400

    landing

---
+++ landing.php
@@ -0,0 +1,431 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+    <HEADER>
+
+        <div id="logo" class="margin-auto">
+        <img class="" src="assets/img/logo3.PNG" style="width: 100%;height: 100%;display: block;" alt="">
+            <!-- <span class="margin-auto__content" style="font-weight: bold;color:white;font-size:24px"> LOGO</span> -->
+
+        </div>
+
+        <div id="cursos">
+
+            <ul>
+                <label for="" style="font-weight: bold;color:#0c2840;font-size:20px">Curso virtual</label>
+                <li><label for="" style="color:#0c2840">lorem</label> </li>
+                <li> <label for="" style="color:#0c2840">lorem</label></li>
+                <li><label for="" style="color:#0c2840">lorem</label> </li>
+            </ul>
+
+        </div>
+
+        <div id="otros" class="margin-auto">
+
+            <span class="margin-auto__content" style="font-weight: bold;color:#0c2840;font-size:24px"> ESPACIO LIBRE</span>
+
+        </div>
+
+
+<div class="dropdown" style="display:none" id="otros1">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+  <i class="bi bi-list"></i>
+
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Cursos</a></li>
+    <li><a class="dropdown-item" href="#">Opcion 1</a></li>
+    <li><a class="dropdown-item" href="#">Opcion 2</a></li>
+    <li><a class="dropdown-item" href="#">Opcion 3</a></li>
+
+  </ul>
+</div>
+
+    </HEADER>
+
+
+    <main>
+        <div class="content ">
+            
+
+                <div class="" style="height:530px">
+                    <img class="" src="assets/img/hombre.PNG" style="width: 100%;height: 100%;display: block;" alt="">
+                </div>
+
+                <div id="" style="height:70px;background:#3ea7c1;position:relative;bottom:0;background-color: #3ea7c1 transparent;">
+
+                    <nav class="navbar ">
+                        <div class="container-fluid">
+                            <ul class="nav navbar-nav" style="margin-top:-32px;height:85px">
+                                <li class="active otro" style=" width:30%;"><a href="#">&nbsp;<i class="bi bi-check2" style="color:orange;font-size:25px;font-weight:bold"></i> &nbsp; &nbsp;Home</a></li>
+                                <li class="otro"><a href="#" style=" width:30%;"><i class="bi bi-check2" style="color:orange;font-size:25px;font-weight:bold"></i> &nbsp; &nbsp;Page 1</a></li>
+                                <li class="otro"><a href="#" style=" width:30%;"><i class="bi bi-check2" style="color:orange;font-size:25px;font-weight:bold"></i> &nbsp; &nbsp;Page 2</a></li>
+                                <li class="otro" style="width:20%;background-color: #ff5a1a"><a href="#" style=" background-color: #ff5a1a; "><i class="bi bi-check2" style="color:orange;font-size:25px;font-weight:bold"></i> &nbsp; &nbsp;Page 3</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
+
+        </div>
+        <aside>
+            <div class="container" style="margin-top: 50px;">
+                <div class="abs-center">
+
+
+                    <form action="#" class=" form">
+                        <div class="form-group text-center" style="margin-bottom: 25px; border-bottom:2px solid white">
+
+                            <label for="" style="font-weight:bold;color:white;font-size:25px">Inscribete </label> <label for="" style="font-weight:bold;color:#ff5a1a;font-size:25px">GRATIS!!!</label>
+
+                        </div>
+                        <div class="form-group" style="margin-bottom: 15px;">
+                            <!-- <label for="email">Email</label> -->
+                            <input type="email" name="email" id="email" placeholder="Nombre" class="form-control">
+                        </div>
+                        <div class="form-group" style="margin-bottom: 15px;">
+                            <!-- <label for="password">Password</label> -->
+                            <input type="password" name="password" id="password" placeholder="Numero de documento" class="form-control">
+                        </div>
+                        <div class="form-group" style="margin-bottom: 15px;">
+                            <!-- <label for="password">Password</label> -->
+                            <input type="password" name="password" id="password" placeholder="Profesion" class="form-control">
+                        </div>
+                        <div class="form-group" style="margin-bottom: 15px;">
+                            <!-- <label for="password">Password</label> -->
+                            <input type="password" name="password" id="password" placeholder="Entidad donde labora " class="form-control">
+                        </div>
+                        <div class="form-group" style="margin-bottom: 15px;">
+                            <!-- <label for="password">Password</label> -->
+                            <input type="password" name="password" id="password" placeholder="Celular" class="form-control">
+                        </div>
+                        <div class="form-group" style="margin-bottom: 15px;">
+                            <!-- <label for="password">Password</label> -->
+                            <input type="password" name="password" id="password" placeholder="Email" class="form-control">
+                        </div>
+                        <div class="form-group" style="margin-bottom: 15px;">
+                            <!-- <label for="password">Password</label> -->
+                            <input type="password" name="password" id="password" class="form-control">
+                        </div>
+                        <div class="text-center" style="margin-top:20px">
+                            <button type="submit" style=" color:white;background:#ff5a1a;width:auto" class="btn btn-primary text-center">Enviar Inscripcion</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </aside>
+
+    </main>
+
+    <article>
+
+        <div style="margin-left:50px">
+
+            <label for="" style="color:#0c2840;font-size:auto;font-weight:bold"><i class="bi bi-mortarboard-fill" style="color:#0c2840;font-size:50px"></i> &nbsp; &nbsp; &nbsp; &nbsp;ESPACIO LIBRE PARA INCLUIR CONTENIDO</label>
+
+
+        </div>
+
+    </article>
+    <section>
+
+        <div class="container text-center" style="margin-top: 10PX;">
+            <div class="row row-cols-4" style="width: 100%;text-align: initial">
+                <div class="col border text-align-left" style=" height:100px;height:100%">
+                    <Label style="font-weight:bold;color:#0c2840">lorem ipsum</Label>
+                    <p style="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+                </div>
+                <div class="col border text-align-left" style=" height:100px;height:fit-content">
+                    <Label style="font-weight:bold;color:#0c2840">lorem ipsum</Label>
+                    <p style="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+
+                </div>
+                <div class="col border text-align-left" style=" height:100px;height:fit-content">
+                    <Label style="font-weight:bold;color:#0c2840">lorem ipsum</Label>
+                    <p style="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+
+                </div>
+                <div class="col border text-align-left" style=" height:100px;height:fit-content">
+                    <Label style="font-weight:bold;color:#0c2840">lorem ipsum</Label>
+                    <p style="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <footer>
+
+        <div id="ft" class="margin-auto">
+
+            <span class="margin-auto__content" style="font-weight: bold;color:white;font-size:18px"> ESPACIO LIBRE INFORMACION DE CONTACTO</span>
+
+        </div>
+
+        <div id="ft2" class="margin-auto">
+
+            <span class="margin-auto__content" style="font-weight: bold;color:#0c2840;font-size:24px"> ESPACIO LIBRE PARA INCLUIR FOOTER</span>
+
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+        list-style: none;
+    }
+
+    body {
+        /* background: url(landing.jpg); */
+        background-repeat: no-repeat;
+
+        background-position: top-center;
+        height: 100%;
+        width: 100%;
+    }
+
+    a {
+        text-decoration: none;
+        font-size: 18px;
+        color: white;
+        font-weight: bold;
+    }
+
+    header {
+
+        position: relative;
+        margin: auto;
+        width: 100%;
+        height: 125px;
+        /* background: aqua; */
+
+    }
+
+
+    main {
+        width: 100%;
+        display: flex;
+        height: 600px;
+
+    }
+
+    main .content {
+        width: 65%;
+        /* background: url(descarga.jpg); */
+        width: 100%;
+        /*blue; */
+        background-repeat: no-repeat;
+        width: 100%;
+        display: block;
+    }
+
+    main aside {
+        width: 35%;
+        /* height: 400px; */
+        background: #0c2840;
+
+    }
+
+    section {
+        position: relative;
+        margin: auto;
+        width: 100%;
+        height: 250px;
+        background: WHITE;
+    }
+
+    footer {
+        position: relative;
+        margin: auto;
+        width: 100%;
+        height: 100px;
+        background: black;
+    }
+
+    article {
+        position: relative;
+        margin: auto;
+        width: 100%;
+        height: 100px;
+        background: hsl(0deg 0% 86%);
+    }
+
+
+    #logo {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        height: 100%;
+        width: 15%;
+        /* background: #0171b7; */
+    }
+
+    #ft {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        height: 100%;
+        width: 35%;
+        background: #0c2840;
+    }
+
+    #ft2 {
+        position: absolute;
+        top: 0px;
+        left: 35%;
+        height: 100%;
+        width: 65%;
+        background: #dbdbdb;
+    }
+
+    #cursos {
+        position: absolute;
+        top: 0px;
+        left: 15%;
+        height: 100%;
+        width: 50%;
+        background: #ffffff;
+    }
+
+    #otros {
+        position: absolute;
+        top: 0px;
+        left: 65%;
+        height: 100%;
+        width: 35%;
+        background: white;
+    }
+
+    #otros1 {
+        /* position: absolute;
+        height: 100%;
+        */
+        /* top: 0px; */
+        left: 85%;
+        width: 10%;
+        height: 25%;
+        background: white; 
+        position:absolute;
+    bottom:5px;
+    right:10px;
+    }
+
+    /* la forma mas facil es ponerle al div 1 position relative y al div 2 position absolute y bottom 0 */
+
+    .margin-auto {
+        display: flex;
+        height: 100px;
+        width: 200px;
+
+        /* background: #ffff99; */
+        color: #333;
+    }
+
+    .margin-auto__content {
+        margin: auto;
+    }
+
+    .abs-center {
+        display: flex;
+        /* align-items: center; */
+        justify-content: center;
+        min-height: 100vh;
+    }
+
+    .form {
+        width: 80%;
+    }
+
+    .otro {
+
+        margin-top: 14px;
+        height: 85px;
+
+    }
+
+    nav ul {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%
+    }
+
+    nav ul li {
+        float: left;
+        width: 25%;
+        top: 18px;
+        position: relative;
+
+    }
+
+    li .active {
+        float: left;
+        color: red;
+        /* color    top:18px; */
+        position: relative;
+
+    }
+
+    @media only screen and (max-width: 600px) {
+
+   
+        .content{
+            display: none!important;
+        }
+
+        #cursos {
+            display: none;
+        }
+
+        #logo {
+            width: 40%;
+        }
+
+        #otros {
+            width: 55%;
+            left: 35%;
+        }
+        #otros1 {
+            display: block!important;
+        }
+
+        main aside {
+            width: 100%
+        }
+
+        .ninguno{
+            display: block!important;
+        }
+
+        section{
+            height: 100%!important;
+        }
+    }
+</style>
